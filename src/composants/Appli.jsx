c92d48e3cc5aa7735c13eb5b75c71dee9d22fe61
Claudia Patry <71995934+Claue-roe@users.noreply.github.com>
2021-03-12 11:13:14 -0500

    Plus de lecture Firebase

--- src/composants/Appli.jsx
+++ src/composants/Appli.jsx
@@ -4,21 +4,39 @@ import Accueil from './Accueil';
 import ListeDossiers from './ListeDossiers';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import {instanceFirestore} from '../firebase';
 
 export default function Appli() {
 
-  const etatUtil = useState(null);
-  const [util,setUtil] = etatUtil;
+  const [util,setUtil] = useState(null);
+
+  useEffect(
+    () => {
+      firebase.auth().onAuthStateChanged(
+        util => {setUtil(util)
+          //Créer le profile de l'utilisateur sdans FireStore si utilisateur n'est pas NULL
+          if(util){
+            instanceFirestore.collection('utilisateurs').doc(util.uid).set({
+              nom: util.displayName,
+              courriel: util.email,
+              datecompte: firebase.firestore.FieldValue.serverTimestamp()
+            }, {merge:true});
+          }
+        }
+      )
+    }, []
+  );
 
   return (
     <div className="Appli">
         {
           util ?
           <>
-          <Entete />
+          <Entete util={util}/>
           <section className="contenu-principal">
-            <ListeDossiers />
+            <ListeDossiers util={util}/>
             <Fab className="ajoutRessource" color="primary" aria-label="Ajouter dossier">
               <AddIcon />
             </Fab>
--- src/composants/ListeDossiers.jsx
+++ src/composants/ListeDossiers.jsx
@@ -1,12 +1,27 @@
 import './ListeDossiers.scss';
-import dossTab from '../data/liste-dossiers.json';
+import {instanceFirestore} from '../firebase'
 import Dossier from './Dossier';
+import { useEffect, useState } from 'react';
 
-export default function ListeDossiers() {
+export default function ListeDossiers({util}) {
+  let [dossiers, setDossiers] = useState([]);
+  useEffect(
+    () => {
+      let dossTab = [];
+      instanceFirestore.collection('utilisateurs').doc(util.uid).collection('dossiers').get().then(
+        reponse => {
+          reponse.forEach(
+          doc => dossTab.push(doc.data()));
+          setDossiers(dossTab);
+        }
+      );
+    }, []
+  )
+  
   return (
     <ul className="ListeDossiers">
       {
-        dossTab.map( 
+        dossiers.map( 
           dossier =>  <li key={dossier.id}><Dossier {...dossier} /></li>
         )
       }
